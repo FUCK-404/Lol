99c0ba8701a1b1ccc7d9706ca8e167e83c97d633
RINKU-CYBER-404 <109244698+RINKU-CYBER-404@users.noreply.github.com>
2022-10-08 07:36:55 +0600

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,74 @@
+#coding=utf-8
+import sys,time
+from platform import uname
+from os import path,system
+arch=uname().machine.lower()
+if "aarch" in arch:
+    arch="aarch"
+    print('033[1;32m\nBypass By Rinku')
+    print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools');time.sleep(1)
+else:
+    print('\033[1;31mSorry System not support this tools');sys.exit()
+    
+banner=("""\033[1;37m    ###    ##    ## #### ##    ##  ###### 🔥
+   ## ##   ##   ##   ##  ###   ## ##    ##    
+  ##   ##  ##  ##    ##  ####  ## ##  
+ ##     ## #####     ##  ## ## ## ##   ####
+ ######### ##  ##    ##  ##  #### ##    ## 
+ ##     ## ##   ##   ##  ##   ### ##    ##  
+ ##     ## ##    ## #### ##    ##  ###### 🔥
+[+]═════════════════════════════════════════
+[+] Author    : IMTIAZ AKING
+[+] Github    : AKING110
+[+] Facebook  : IMTIAZ.AKING.07
+[+] Tool Type : Bypass
+[+] Version   : 1.3.6
+[+] this massage for haters : \033[1;31mjust feel me 🔥
+\033[1;37m[+]═════════════════════════════════════════""")
+
+def main():
+    if path.isfile("dz.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/dz.so -o dz.so")
+    system('clear')
+    print(banner)
+    print('[1] Version : 1.3.6 (new)\n[2] Version : 1.3.5 (working)\n[3] Version : 1.3.4 (old)\n[4] Version : 1.3.3 (old)\n[5] Random Cloning (new)\n\033[1;37m[+]═════════════════════════════════════════')
+    vs=input('[•] Choice : ')
+    if vs in ['1','01']:
+        if path.isfile("File.so"):
+            import File
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/File.so -o File.so")
+            import File
+    elif vs in ['2','02']:
+        if path.isfile("AKINGG.so"):
+            import AKINGG
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKINGG.so -o AKINGG.so")
+            import AKINGG
+    elif vs in ['3','03']:
+        if path.isfile("AKING64.so"):
+            import AKING
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKING64.so -o AKING64.so")
+            import AKING64
+    elif vs in ['4','04']:
+        if path.isfile("AKING.so"):
+            import AKING64
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/AKING.so -o AKING.so")
+            import AKING
+    elif vs in ['5','05']:
+        if path.isfile("Rndm.so"):
+            import Rndm
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/Rndm.so -o Rndm.so")
+            import Rndm
+    else:
+        if path.isfile("File.so"):
+            import File
+        else:
+            system("curl -L https://raw.githubusercontent.com/AKING110/Data/main/File.so -o File.so")
+            import File
+main()
